2c1ad6b3cf59244ccc6b8931bdb62e715ed69d6f
Janis Miezitis <janjiss@gmail.com>
2013-03-17 15:01:05 +0200

    Initia;

---
+++ spec/lib/push_list_spec.rb
@@ -0,0 +1,7 @@
+require "spec_helper"
+
+describe PushList do
+  it "should intitialize" do
+    PushList.new("janjiss").should be_true
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,2 @@
+require "rspec"
+require "../lib/push_list"
